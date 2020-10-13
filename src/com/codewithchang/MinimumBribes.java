33d9f87f03e7e5ed36139fdb01fca46560cbef0c
Chang <cnxiong03@gmail.com>
2020-10-12 22:54:53 -0400

    counting how many bribes

---
+++ src/com/codewithchang/MinimumBribes.java
@@ -0,0 +1,14 @@
+package com.codewithchang;
+
+public class MinimumBribes {
+    static void minimumBribes(int[] q) {
+        int swap = 0;
+        int bribes;
+        int pos = 0;
+        for(int i = q.length-1; i >= 0; i--) {
+            int j = 0;
+
+            bribes = q[]
+        }
+    }
+}
